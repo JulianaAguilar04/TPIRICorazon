59ab66a48105504d3fb071630cdf356f1d6943d2
JulianaAguilar04 <aguilariuzchuk.juliana@gmail.com>
2024-11-01 08:53:53 -0300

    siii

--- TP IRI CORAZON.py
+++ TP IRI CORAZON.py
@@ -1,1 +1,12 @@
-print("Hello world")
+# estado del corazon
+est_corazon = { 
+    "p_sistolica": 0,
+    "p_diastolica": 0,
+    "volumen": 0,
+    "frecuencia":0,
+}
+
+
+#simulacion de estados
+normal = {120, 80,100,2030 }
+correr = {140, 80, 120, 160}
